c745799642c74f13c74c68f84b1b43f84145a62b
Almeida-91 <aplalmeida91@outlook.com>
2022-07-09 16:04:36 +0100

    Add basic calculator functionality

--- script.js
+++ script.js
@@ -31,3 +31,53 @@ function operate(operator, intA, intB){
             return divide(intA, intB);
     }
 }
+
+const numberbuttons = document.getElementsByClassName(`number`);
+const operators = document.getElementsByClassName(`operator`);
+const display = document.getElementById(`display`);
+const clearbutton = document.getElementById(`clear`);
+const history = document.getElementById(`history`);
+
+for (i=0 ; i < numberbuttons.length; i++){
+    numberbuttons[i].addEventListener(`click`, function(e){
+        display.textContent += e.target.textContent;
+    });
+}
+
+for (i=0 ; i < operators.length ; i++){
+    operators[i].addEventListener(`click`, function(e){
+        let operator = hasOperator(history.textContent);
+        if(operator == false){
+            history.textContent += display.textContent+e.target.textContent;
+        } else {
+            let intA = history.textContent.slice(0,-1);
+            display.textContent = operate(operator.toString(),intA,display.textContent);
+            history.textContent = '';
+            history.textContent += display.textContent+e.target.textContent;
+        }
+        clearDisplay();
+    })
+}
+
+clear();
+
+clearbutton.addEventListener(`click`, clear);
+
+function clearDisplay(){
+    display.textContent = '';
+}
+
+function clear(){
+    display.textContent = '';
+    history.textContent = '';
+}
+
+function hasOperator(string){
+    string = string.split('');
+    for(let i=0 ; i<string.length ; i++){
+        if (string[i]== `+` || string[i] == '-' || string[i] == '*' || string[i] == '/'){
+            return string[i];
+        }
+    }
+    return false;
+}

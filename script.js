0914e87c0c52c497f51938cca8b4ca3a6fa68427
Almeida-91 <aplalmeida91@outlook.com>
2022-07-10 13:50:29 +0100

    Add keyboard support, bug fixes, edit css

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ const backspace = document.getElementById(`backspace`);
 let result = NaN;
 let initialValue = '';
 let operator = '';
+let pass = 0;
 
 for (i=0 ; i < numberbuttons.length; i++){
     numberbuttons[i].addEventListener(`click`, function(e){
@@ -76,7 +77,7 @@ for (i=0 ; i < operators.length ; i++){
         }
         if (initialValue == '') initialValue = display.textContent;
         if (operator == '' && display.textContent != '') initialValue = display.textContent;
-        if(operator == ''){
+        if (operator == ''){
             operator = e.target.textContent;
             clear();
         } else if(result == display.textContent){
@@ -119,10 +120,7 @@ function hasOperator(string){
 equals.addEventListener(`click`, getResult);
 
 function getResult(){
-    if (initialValue == '') initialValue = display.textContent;
-    console.log(`left : ${initialValue}`);
-    console.log(`operator : ${operator}`);
-    console.log(`right : ${display.textContent}`);
+    if (initialValue == '' && display.textContent!= result) initialValue = display.textContent;
     if(display.textContent == 'ERROR'){
         clearDisplay();
         return;
@@ -134,7 +132,17 @@ function getResult(){
         clearDisplay();
     } else {
         result = operate(operator.toString(),initialValue,display.textContent);
-        if(isDecimal(result.toString())) result = result.toFixed(5);
+        if(isDecimal(result.toString())){
+            pass = 0;
+            for( let i = 1 ; i < 100000 ; i = i*10){
+                if (isDecimal((+result*i).toString())==false){
+                    result = +result.toFixed(pass);
+                    break;
+                }
+                pass++;
+            }
+            result = result.toFixed(pass);
+        }
         display.textContent = result;
         initialValue = '';
         result = display.textContent;
@@ -142,8 +150,7 @@ function getResult(){
 }
 
 function isDecimal(string){
-    string = string.split('')
-    for (i = 0 ; i<string.length ; i++){
+    for ( let i = 0 ; i<string.length ; i++){
         if (string[i] == '.') return true;
     }
     return false;
@@ -158,3 +165,39 @@ decimal.addEventListener(`click`, function (e){
 backspace.addEventListener(`click`, function(){
     display.textContent = display.textContent.slice(0,-1);
 })
+
+
+//add keyboard support
+
+document.addEventListener(`keydown`, (e)=>{
+    if (e.code == 'Backspace' || e.code == 'Delete'){
+        if (display.textContent == result) clearbutton.click();
+        else backspace.click();
+    }
+    if (e.code == 'Escape') clearbutton.click();
+})
+
+document.addEventListener(`keypress`, (e)=>{
+    if (e.code == `NumpadDecimal` || e.code == `Period` ){
+        decimal.click();    
+    } else if ((e.code == `NumpadEnter` || e.code == 'Enter' || e.code == `Equal`) && operator != ''){
+        equals.click();
+    } else if (e.key == `+` || e.key == '-' || e.key == '*' || e.key == '/' || e.key == '^'){
+        for(let i = 0 ; i < operators.length ; i++){
+            if (e.key == `${operators[i].textContent}`) operators[i].click();
+        }
+    }
+    else {
+        for (i = 0 ; i<10 ; i++){
+            if (e.code == `Numpad${i}` || e.code == `Digit${i}`){
+                if (initialValue == 'ERROR' || display.textContent == 'ERROR') clearDisplay();
+                if (result == display.textContent){
+                    clear();
+                    result = NaN;
+                }
+                display.textContent += `${i}`;
+            };
+        };
+    };
+});
+
